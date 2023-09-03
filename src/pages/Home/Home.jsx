39a84d7c15c61a1961570f3b412f36058b25843b
Irulik <123405063+Irulik@users.noreply.github.com>
2023-09-03 19:02:50 +0200

    Fix of bug

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,24 @@
 import FilmsList from '../../components/FilmsList/FilmsList'
 import { fetchTrendingApi } from '../../api/fetchApi';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState, useMemo  } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Home = () => {
 	const [movies, setMovies] = useState([]);
 	const location = useLocation();
-    const navigationHistory = JSON.parse(sessionStorage.getItem('navigationHistory')) || [];
 
 	useEffect(() => {
 		fetchTrendingApi(3).then(setMovies);
+	}, []);
+
+	const navigationHistory = useMemo(() => {
+	    return JSON.parse(sessionStorage.getItem('navigationHistory')) || [];
 	}, []);
 
 	useEffect(() => {
 	    navigationHistory.push(location.pathname);
 	    sessionStorage.setItem('navigationHistory', JSON.stringify(navigationHistory));
 	}, [location.pathname, navigationHistory]);
-
 
 	return (
 		<>
