885617b720575241f05fda378c6979031af906c4
Irulik <123405063+Irulik@users.noreply.github.com>
2023-09-07 08:42:19 +0200

    updated

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -115,33 +115,3 @@ const Cast = () => {
     );
 };
 export default Cast;
-
-
-
-// const Cast = ({ id }) => {
-//     const [cast, setCast] = useState([]);
-
-//     useEffect(() => {
-//         fetchCast(id).then((data) => {
-//             setCast(data.cast);
-//         });
-//     }, [id]);
-//     // console.log(cast);
-//     // const top20Cast = cast.slice(0, 20); //на випадок обмеження кількості акторів
-//     return (
-//         cast.map((person, index) => (
-//             <CastPerson key={index}>
-//                 <CastImg>
-//                     <img
-//                         src={person.profile_path
-//                             ? `https://image.tmdb.org/t/p/w500/${person.profile_path}`
-//                             : 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700'}
-//                         alt={person.name} />
-//                 </CastImg>
-//                 <CastActor>Actor: {person.name}</CastActor>
-//                 <CastCharacter>Character: {person.character}</CastCharacter>
-//             </CastPerson>
-//         ))
-//     );
-// };
-// export default Cast;
